f7cf42e60cd6ad6cf71ef057c6b07d654eec5edc
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-01-19 22:53:51 +0000

    implemented lambda functions (the only thing i learnt from big data)

--- main.py
+++ main.py
@@ -6,8 +6,27 @@ def readClipboard():
 def writeClipboard(text):
     pc.copy(text);
 
+def optimizer(text):
+    punc = ".,:;%#/"
+    text = text.split("\n");
+    text = list(filter(lambda x: x != " " and x != "", text)); # remove whitespace and empty elements
+
+    i = 0;
+    while (i < len(text)):
+        if "<" in text[i] or ">" in text[i]:
+            i += 1;
+        else:
+
+
+    return text;
+
 def main():
-    print("Hello World!");
+    text = readClipboard();
+    while(text == ""):
+        text = readClipboard();
+    
+    print(optimizer(text));
+
 
 if __name__ == "__main__":
     main();
