c50a97dc41b24521326371d78e9b065aa8d0d42c
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-01-19 23:33:57 +0000

    money moves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyperclip as pc
+from bs4 import BeautifulSoup as bs
 
 def readClipboard():
     return pc.paste();
@@ -9,36 +10,35 @@ def writeClipboard(text):
 def optimizer(text):
     punc = ".,:;%#/"
     text = text.split("\n");
-    text = list(filter(lambda x: x != "", text)); # removes empty elements
+
+    text = map(str.strip, text)  # removes whitespace from each element (if element is purely whitespace, it becomes an empty string)
+    text = list(filter(lambda x: x != "", text)); # removes empty strings
 
     i = 0;
     while (i < len(text)):
-        text[i] = text[i].strip();
+        """TODO:
+        1. remove all elements that are comments [DONE]
+        2. remove last set of lines:
+            a. final closing curly bracket "}" [DONE]
+            b. closing style tag "</style>" [DONE]
+        3. remove last semicolon inside each class [DONE]
+        4. remove all whitespaces in between valid punctuation
+        5. remove units for width and height (EG: width: 100px -> width: 100)
+        6. remove all newlines"""
 
-        if (text[i].isspace()):
+        if ("<!--" in text[i]): # removing comments
             text.pop(i);
+            i -= 1; # since idx will be offset by +1
+        
+        # removing last semicolon in each class:
+        if (text[i] == "}"):
+            text[i-1] = text[i-1][:-1]; # remove last character
+        
+        if (text[i] == "</style>"): # slice list from before this element
+            text = text[:i-1];
 
-        if ("<" in text[i] or ">" in text[i]): #if element is a html tag
-            i += 1;
 
-        else: # main optimization loop
-            
-            """TODO:
-            1. remove all elements that are comments
-            2. remove last set of lines:
-                a. final class semicolon
-                b. final closing curly bracket "}
-                c. closing style tag "</style>"
-                d. all comments below that [DONE]
-            3. remove all whitespaces in between valid punctuation
-            4. remove last semicolon inside each class
-            5. remove all newlines"""
-
-            if ("<!--" in text[i]) { # removing comments
-                text.pop(i);
-            }
-
-        i += 1;   
+        i += 1;
 
     return text;
 
