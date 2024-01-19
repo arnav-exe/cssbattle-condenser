f186fa5433c8075e7109c42492e703bc8110792c
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-01-19 23:15:29 +0000

    text cleansing + comment line removal

--- main.py
+++ main.py
@@ -9,14 +9,36 @@ def writeClipboard(text):
 def optimizer(text):
     punc = ".,:;%#/"
     text = text.split("\n");
-    text = list(filter(lambda x: x != " " and x != "", text)); # remove whitespace and empty elements
+    text = list(filter(lambda x: x != "", text)); # removes empty elements
 
     i = 0;
     while (i < len(text)):
-        if "<" in text[i] or ">" in text[i]:
-            i += 1;
-        else:
+        text[i] = text[i].strip();
 
+        if (text[i].isspace()):
+            text.pop(i);
+
+        if ("<" in text[i] or ">" in text[i]): #if element is a html tag
+            i += 1;
+
+        else: # main optimization loop
+            
+            """TODO:
+            1. remove all elements that are comments
+            2. remove last set of lines:
+                a. final class semicolon
+                b. final closing curly bracket "}
+                c. closing style tag "</style>"
+                d. all comments below that [DONE]
+            3. remove all whitespaces in between valid punctuation
+            4. remove last semicolon inside each class
+            5. remove all newlines"""
+
+            if ("<!--" in text[i]) { # removing comments
+                text.pop(i);
+            }
+
+        i += 1;   
 
     return text;
 
